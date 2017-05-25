e7063798dd8c35a0809a26ff3c435a972162b9b9
IMDroidude <zareahmer89@gmail.com>
2017-05-25 19:47:20 +0500

    my first commit

---
+++ app/src/main/java/droidudes/test/project/SplashyActivity.java
@@ -0,0 +1,13 @@
+package droidudes.test.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashy);
+    }
+}
